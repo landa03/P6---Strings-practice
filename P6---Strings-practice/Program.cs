7a52c77a4157d55729616187a32c4030dcee2963
landa03 <60487799+landa03@users.noreply.github.com>
2020-04-29 10:30:36 -0700

    Add files via upload

---
+++ P6---Strings-practice/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace P6___Strings_practice
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string motivacion = "ave, banco, comida, duda, ejercicio, fuente, girasol";
+            motivacion = motivacion.ToUpper();
+            motivacion = motivacion.Replace(",", ";");
+            motivacion = motivacion.Replace(" ", "");
+
+            string[] motivacionArray = motivacion.Split(";");
+           
+            System.Console.WriteLine(String.Format("{5};{4};{3};{2};{1}", motivacionArray));
+        }
+    }
+}
